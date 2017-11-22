e1ffcf0a5939af60d87ab09809aae2f10eaa1be6
Olivier Ligot <oligot@gmail.com>
2017-11-22 10:30:47 +0100

    Initial commit

---
+++ custom-d3.js
@@ -0,0 +1,17 @@
+export {
+  scaleBand,
+  scaleLinear
+} from "d3-scale";
+
+export {
+  axisBottom,
+  axisLeft
+} from "d3-axis";
+
+export {
+  select
+} from "d3-selection";
+
+export {
+  max
+} from "d3-array";
